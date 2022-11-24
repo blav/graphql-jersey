d302ede659e43ef787b2317bc67d84c24d3b84f5
David Blavier <blav@actar.us>
2022-11-24 14:09:33 +0100

    moved task schema to fragment

--- src/main/java/com/sciforma/lab/graphql/modules/demo/task/TaskGraphQLModule.java
+++ src/main/java/com/sciforma/lab/graphql/modules/demo/task/TaskGraphQLModule.java
@@ -12,26 +12,17 @@ import static com.sciforma.lab.graphql.modules.demo.project.ProjectGraphQLModule
 import static graphql.language.FieldDefinition.newFieldDefinition;
 import static graphql.language.ListType.newListType;
 import static graphql.language.NonNullType.newNonNullType;
-import static graphql.language.ObjectTypeDefinition.newObjectTypeDefinition;
 import static graphql.language.TypeName.newTypeName;
 
 @Service
 @Priority (20)
-@ModuleComponents (dataFetchers = TasksDataFetcher.class)
+@ModuleComponents (dataFetchers = TasksDataFetcher.class, schemaFragments = "task.graphqls")
 public class TaskGraphQLModule extends AbstractGraphQLModule {
 
   public static final String TYPE_TASK = "Task";
 
   @Override
   public void provideTypes (TypeDefinitionRegistry registry, Builder runtimeWiring) {
-    registry.add (newObjectTypeDefinition ()
-      .name (TYPE_TASK)
-      .fieldDefinition (newFieldDefinition ()
-        .name ("id")
-        .type (newNonNullType (newTypeName ("ID").build ()).build ())
-        .build ())
-      .build ());
-
     transform (registry, TYPE_PROJECT, builder -> builder
       .fieldDefinition (newFieldDefinition ()
         .name ("tasks")
